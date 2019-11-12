1bdc3b0b7a5f3408f59407af2e9e1337b7b840f6
Mateusz Morawiec <mateuszmorawiec98@wp.pl>
2019-11-12 10:27:26 +0100

    Frontend small modifications

--- Ceneo/app.py
+++ Ceneo/app.py
@@ -8,8 +8,6 @@ def home():
     if request.method == "POST":
         # pobranie wszystkich zmiennych z input
         data = {"tab": [request.form[f'productName{i}'] for i in range(1, 6)]}
-        # tab.append(request.form["productName1"])
-        # tab.append(request.form["productName2"])
 
         # wykonanie funkcji
         return render_template("output.html", context=data)
@@ -17,14 +15,14 @@ def home():
         return render_template("input.html")
 
 
-@app.route("/output", methods=["POST", "GET"])
-def output(out):
-    return render_template("output.html", content=out)
-
-
-@app.route("/<nm1>")
-def user(nm1, nm2):
-    return render_template("output.html", content1=nm1, content2=nm2)
+# @app.route("/output", methods=["POST", "GET"])
+# def output(out):
+#     return render_template("output.html", content=out)
+#
+#
+# @app.route("/<nm1>")
+# def user(nm1, nm2):
+#     return render_template("output.html", content1=nm1, content2=nm2)
 
 
 if __name__ == "__main__":
