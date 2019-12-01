9cbf7c2cb710a0b9151dcdf5e1a5e45ce089feb1
Mateusz Morawiec <mateuszmorawiec98@wp.pl>
2019-12-01 23:34:34 +0100

    Input data validation - all fields required.

--- Ceneo/static/scripts/search.js
+++ Ceneo/static/scripts/search.js
@@ -5,25 +5,37 @@ function validateForm(){
         var quantity = parseInt(document.forms["inputForm"]["quantity" + i].value);
         var minPrice = parseInt(document.forms["inputForm"]["minPrice" + i].value);
         var maxPrice = parseInt(document.forms["inputForm"]["maxPrice" + i].value);
+        var reputation = parseInt(document.forms["inputForm"]["reputation" + i].value);
 
         if (!productName){
             emptyFields++;
         }
 
         if (!productName && quantity > 0){
-            displayAlert("Enter the name of product no " + (i+1) + " or set quantity to 0.");
+            displayAlert("Enter the name of product No. " + (i+1) + " or set quantity to 0.");
             return false;
         }
 
         if (productName && !quantity){
-            displayAlert("Enter the quantity of product no " + (i+1) + ".");
+            displayAlert("Enter the quantity of product No. " + (i+1) + ".");
             return false;
         }
 
-        if ((minPrice > maxPrice) && maxPrice){
-            displayAlert("Minimum price of product no " + (i+1) + " is higher than maximum.");
+        if (productName && quantity && (!minPrice || !maxPrice)){
+            displayAlert("Enter the price range for product No. " + (i+1) + ".");
             return false;
         }
+
+        if (minPrice > maxPrice){
+            displayAlert("Minimum price of product No. " + (i+1) + " is higher than maximum.");
+            return false;
+        }
+
+        if (productName && quantity && !reputation){
+            displayAlert("Enter the minimal store reputation for product No. " + (i+1) + ".");
+            return false;
+        }
+
     }
 
     if(emptyFields == 5){
