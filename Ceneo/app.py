b43348e26a6239314ad939c92ff396bfd41c7022
Mateusz Morawiec <mateuszmorawiec98@wp.pl>
2019-12-03 17:40:05 +0100

    Small frontend corrections. Comments added.

--- Ceneo/app.py
+++ Ceneo/app.py
@@ -4,35 +4,35 @@ import json
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/")     # Home page (127.0.0.1:5000)
 def home():
     return render_template("home.html")
 
 
-@app.route("/search", methods=["POST", "GET"])
+@app.route("/search", methods=["POST", "GET"])      # Search page (127.0.0.1:5000/search)
 def search():
     if request.method == "POST":
-        data = {'products': []}
+        data = {'products': []}     # Dictionary for input data
         for i in range(5):
             if request.form[f'productName{i}']:
                 data['products'].append({
-                    'name': request.form[f'productName{i}'],
-                    'quantity': request.form[f'quantity{i}'],
+                    'name': request.form[f'productName{i}'],    # Get the value of the form field named "productName{i}"
+                    'quantity': request.form[f'quantity{i}'],   # "productName{i}" means "productName1" for i=1 etc
                     'minimum_price': request.form[f'minPrice{i}'],
                     'maximum_price': request.form[f'maxPrice{i}'],
                     'reputation': request.form[f'reputation{i}']
                 })
         with open('input_data.txt', 'w') as file:
-            json.dump(data, file)
+            json.dump(data, file)                   # Save input data to json file
         ## search function
         with open('output_data.txt') as json_file:
-            output_data = json.load(json_file)
+            output_data = json.load(json_file)      # Load output data from json file
         return render_template("results.html", context=output_data)
     else:
         return render_template("search.html")
 
 
-@app.route("/authors")
+@app.route("/authors")      # Authors page (127.0.0.1:5000/authors)
 def authors():
     return render_template("authors.html")
 
