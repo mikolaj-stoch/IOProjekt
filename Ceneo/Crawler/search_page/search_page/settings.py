860e10532343b1ca9b6eb2455198a54bf1f033d5
miko083 <mikolajstoch@gmail.com>
2019-12-12 12:41:34 +0100

    Added logs

--- Ceneo/Crawler/search_page/search_page/settings.py
+++ Ceneo/Crawler/search_page/search_page/settings.py
@@ -88,3 +88,6 @@ ITEM_PIPELINES = {
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+LOG_STDOUT = True
+LOG_FILE = '../Logs/scrapy_output_search_page.txt'
